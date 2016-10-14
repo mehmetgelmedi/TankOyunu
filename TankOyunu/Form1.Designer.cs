c2f8c48048172e55aab0f777e0cc767d4fec6721
mehmetgelmedi <mehmetemingelmedi@gmail.com>
2016-10-14 22:42:15 +0300

    ilk dosyalar eklendi.

---
+++ TankOyunu/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace TankOyunu
+{
+    partial class frmTank
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dgvPanel = new System.Windows.Forms.DataGridView();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPanel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPanel
+            // 
+            this.dgvPanel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPanel.Location = new System.Drawing.Point(12, 12);
+            this.dgvPanel.Name = "dgvPanel";
+            this.dgvPanel.Size = new System.Drawing.Size(30, 30);
+            this.dgvPanel.TabIndex = 3;
+            this.dgvPanel.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPanel_CellClick);
+            this.dgvPanel.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.dgvPanel_CellPainting);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(418, 12);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "Oluştur";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(418, 42);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Başla";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // frmTank
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 334);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.dgvPanel);
+            this.Controls.Add(this.btnCreate);
+            this.Name = "frmTank";
+            this.Text = "Tank";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPanel)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPanel;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ TankOyunu/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TankOyunu
+{
+    public partial class frmTank : Form
+    {
+        Image image;
+        public frmTank()
+        {
+            InitializeComponent();
+            timer1.Interval = 300;    
+            //image=Image.FromFile()
+        }
+        int x = 10, y = 10, curX = 0, curY = 0;
+        string komut = "1", sag = "→", sol = "←", ust = "¯", alt = "_", curYon = "", curFirca = "";
+        char yildiz = '*';
+        int bos = 0;
+        char[] arr;
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            dgvPanel.ColumnCount = x;
+            dgvPanel.RowCount = y;
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    dgvPanel.Rows[i].Cells[j].Value = bos;
+                }
+            }
+            dgvPanel.Rows[curY].Cells[curX].Value = sag + ust;
+
+            dgvPanel.ColumnHeadersVisible = false;
+            dgvPanel.RowHeadersVisible = false;
+            dgvPanel.AutoSize = true;
+            dgvPanel.AutoResizeColumns();
+            dgvPanel.AutoResizeRows();
+            arr = komut.ToCharArray();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int iX = 1, iY = 1;
+            if (curYon == sag && curX != 9)
+            {
+                dgvPanel.Rows[curY].Cells[curX].Value =1;
+                curX += iX;
+                dgvPanel.Rows[curY].Cells[curX].Value = curYon + curFirca;
+            }
+            if (curYon == sol && curX != 0)
+            {
+                dgvPanel.Rows[curY].Cells[curX].Value =  1;
+                curX -= iX;
+                dgvPanel.Rows[curY].Cells[curX].Value = curYon + curFirca;
+            }
+            if (curYon == alt && curX != 9)
+            {
+                dgvPanel.Rows[curX].Cells[curY].Value = 1;
+                curX += iY;
+                dgvPanel.Rows[curX].Cells[curY].Value = curYon + curFirca;
+            }
+            if (curYon == ust && curX != 0)
+            {
+                dgvPanel.Rows[curX].Cells[curY].Value = 1;
+                curX -= iY;
+                dgvPanel.Rows[curX].Cells[curY].Value = curYon + curFirca;
+            }
+            if (curX == 9)
+                curYon = sol;
+            if (curX == 0)
+                curYon = sag;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // engel durumuna göre aşağıda işlem yapılacak
+            curYon = sag;
+            timer1.Enabled = true;
+            timer1.Start();
+        }
+
+        private void dgvPanel_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void dgvPanel_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            // if ((char)dgvPanel.Rows[e.RowIndex].Cells[e.ColumnIndex].Value =='0')
+            //{
+            //    dgvPanel.Rows[e.RowIndex].Cells[e.ColumnIndex].
+            //}
+        }
+    }
+}
